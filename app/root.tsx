f57823ce91342b386502b86a76fe96adf9894193
js62789 <js62789@gmail.com>
2024-07-18 12:53:02 -0400

    Add socket.io branch

--- app/root.tsx
+++ app/root.tsx
@@ -6,14 +6,19 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import { cssBundleHref } from '@remix-run/css-bundle';
-import './root.css';
 import type { LinksFunction } from '@remix-run/node';
+import { useEffect, useState, type ReactNode } from 'react';
+import type { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
+
+import { SocketProvider } from './context';
+import './root.css';
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ];
 
-export default function App() {
+export function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -24,10 +29,36 @@ export default function App() {
       </head>
       <body>
         <h1>Hello world!</h1>
-        <Outlet />
+        {children}
+
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  const [socket, setSocket] = useState<Socket>();
+
+  useEffect(() => {
+    const socket = io();
+    setSocket(socket);
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on('confirmation', (data) => {
+      console.log(data);
+    });
+  }, [socket]);
+
+  return (
+    <SocketProvider socket={socket}>
+      <Outlet />
+    </SocketProvider>
   );
 }
