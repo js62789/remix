b53d8a02f1da4da870e96abf25207ccf1ff80149
js62789 <js62789@gmail.com>
2024-07-18 14:01:51 -0400

    Add title

--- app/root.tsx
+++ app/root.tsx
@@ -7,11 +7,17 @@ import {
 } from '@remix-run/react';
 import { cssBundleHref } from '@remix-run/css-bundle';
 import './root.css';
-import type { LinksFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ];
+
+export const meta: MetaFunction = () => ([
+  {
+    title: 'My Remix App',
+  },
+]);
 
 export default function App() {
   return (
