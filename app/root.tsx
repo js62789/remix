1d010ef040d11f07357849a768d447659891bc7a
js62789 <js62789@gmail.com>
2024-08-08 22:10:39 -0400

    Add error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,15 @@
 import {
+  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from '@remix-run/react';
 import { cssBundleHref } from '@remix-run/css-bundle';
 import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import { type ReactNode } from 'react';
 import './root.css';
 
 export const links: LinksFunction = () => [
@@ -23,7 +26,9 @@ export const meta: MetaFunction = () => ([
   },
 ]);
 
-export default function App() {
+// The Layout will render even if an error was caught by an error boundary,
+// so make sure nothing can break in here
+export function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -33,10 +38,50 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>
+          {error.status} 
+          {' '}
+          {error.statusText}
+        </h1>
+        <p>
+          {error.data}
+        </p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>
+          {error.message}
+        </p>
+        <p>The stack trace is:</p>
+        <pre>
+          {error.stack}
+        </pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
+}
+
+export default function App() {
+  return (
+    <Outlet />
   );
 }
